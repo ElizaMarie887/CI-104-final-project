be564f2aea6d71b0cfe3e53e341d577c31f329ef
Denise "Eliza" Cherry-Russell <108440338+ElizaMarie887@users.noreply.github.com>
2022-12-06 14:42:12 -0600

    Saving Progress

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-
-
 //Travel Itinerary Section
 
 //Query Selectors
-let addTravelButton = document.querySelector('#addTravelButton')
-let travelText = document.querySelector('#travelText')
-let emptyTravelList = document.querySelector('#ulTravelList')
+let addTravelButton = document.querySelector('#addTravelButton');
+let travelText = document.querySelector('#travelText');
+let emptyTravelList = document.querySelector('#ulTravelList');
 
 // Event Listener to add travel
 
@@ -24,8 +13,13 @@ addTravelButton.addEventListener('click', function(event){
     //console.log("event working")
     let travelItem = document.createElement('li');
     travelItem.innerText = travelText.value;
-    ulTravelList.appendChild(travelItem)
-    travelText.value = ""
+    ulTravelList.appendChild(travelItem);
+    travelText.value = "";
+})
+
+emptyTravelList.addEventListener('click', function (event){
+    let element = event.target
+    element.style.textDecoration = "line-through"
 })
 
 
@@ -33,19 +27,25 @@ addTravelButton.addEventListener('click', function(event){
 
 //Query Selectors
 
-let addPlaceMeetingButton = document.querySelector('#addPlacesMeetingButton')
-let placeMeetingText = document.querySelector('#placesMeetingText')
-let emptyPlacesMeetingList = document.querySelector('#ulPlacesMeetings')
+let addPlaceMeetingButton = document.querySelector('#addPlacesMeetingButton');
+let placeMeetingText = document.querySelector('#placesMeetingText');
+let emptyPlacesMeetingList = document.querySelector('#ulPlacesMeetings');
 
 //Event Listener to add Places and Meetings
 
-addPlaceMeetingButton.addEventListener('click', function (){
+addPlaceMeetingButton.addEventListener('click', function (event){
+    event.preventDefault();
     //want to add text to currently empty list
     //console.log("event working")
-    let placeMeetingItem = document.createElement ('li')
+    let placeMeetingItem = document.createElement ('li');
     placeMeetingItem.innerText = placeMeetingText.value;
     emptyPlacesMeetingList.appendChild(placeMeetingItem);
-    placeMeetingText.value = ""
+    placeMeetingText.value = "";
+})
+
+emptyPlacesMeetingList.addEventListener('click', function (event){
+    let element = event.target
+    element.style.textDecoration = "line-through"
 })
 
 
@@ -53,16 +53,22 @@ addPlaceMeetingButton.addEventListener('click', function (){
 
 //Query Selectors
 
-let addPackingListButton = document.querySelector('#addPackingListButton')
-let packingListText = document.querySelector('#packingListText')
-let emptyPackingList = document.querySelector('#ulPackingList')
+let addPackingListButton = document.querySelector('#addPackingListButton');
+let packingListText = document.querySelector('#packingListText');
+let emptyPackingList = document.querySelector('#ulPackingList');
 
 //Event Listener to add items and line through
 
-addPackingListButton.addEventListener('click', function(){
+addPackingListButton.addEventListener('click', function(event){
+    event.preventDefault();
     //want to add items to the current empty list
     let packingItem = document.createElement('li');
     packingItem.innerText = packingListText.value;
     emptyPackingList.appendChild(packingItem);
-    packingListText.value = ""
+    packingListText.value = "";
+})
+
+emptyPackingList.addEventListener('click', function (event){
+    let element = event.target
+    element.style.textDecoration = "line-through"
 })
